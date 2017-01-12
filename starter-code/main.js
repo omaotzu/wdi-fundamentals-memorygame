37d97793966d3bcd2000aa97c4f2a4fe7df7134c
OmaoTzu <OmaoTzu@Irinas-MBP-2.home>
2017-01-12 14:39:31 +0000

    wdi-fundamentals-memorygame

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,10 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo==cardOne) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry try again");
+}
