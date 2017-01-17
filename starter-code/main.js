adff2878d3199e77deaafa8b4af128296d183ff5
OmaoTzu <OmaoTzu@Irinas-MBP-2.home>
2017-01-17 15:12:18 +0000

    memory game

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,11 +1,28 @@
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay [];
+
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo==cardOne) {
+/* if (cardTwo==cardOne) {
 	alert("You found a match!");
 }
 else {
 	alert("Sorry try again");
-}
+} 
+*/
+
+
+var boardDiv = document.getElementById('game-board');
+function createBoard (){
+		for (var i=0 ; i<cards.length ; i++) {
+			var cardDiv = document.createElement('div');
+			cardDiv.className = 'card';
+			boardDiv.appendChild(cardDiv);
+			boardDiv.appendChild(cardDiv);
+	}
+};
+
+createCards();
