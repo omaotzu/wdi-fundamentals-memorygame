5a7acc3577b36e0bca7fba720467b716d5596f6c
OmaoTzu <OmaoTzu@Irinas-MBP-2.home>
2017-01-19 16:01:20 +0000

    memory game

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,10 +1,5 @@
 var cards = ['queen', 'queen', 'king', 'king'];
-var cardsInPlay [];
-
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardsInPlay = [];
 
 /* if (cardTwo==cardOne) {
 	alert("You found a match!");
@@ -15,14 +10,40 @@ else {
 */
 
 
-var boardDiv = document.getElementById('game-board');
-function createBoard (){
+var board = document.getElementById('gameBoard');
+function createCards (){
 		for (var i=0 ; i<cards.length ; i++) {
 			var cardDiv = document.createElement('div');
 			cardDiv.className = 'card';
-			boardDiv.appendChild(cardDiv);
-			boardDiv.appendChild(cardDiv);
+			cardDiv.setAttribute('data-card', cards[i]);
+			cardDiv.addEventListener('click', isTwoCards);
+			board.appendChild(cardDiv);
 	}
-};
+}
 
-createCards();
+
+
+function isTwoCards(){
+	cardsInPlay.push(this.getAttribute('data-card'));
+		if (this.getAttribute('data-card')==='queen') {
+			this.innerHTML="<img src='http://i.imgur.com/eO9Qt1K.png'>";
+		}
+		else {
+			this.innerHTML="<img src = 'http://i.imgur.com/gExrkx4.png'>";
+		}
+		if(cardsInPlay.length===2) {
+		isMatch(cardsInPlay);
+		cardsInPlay= [];
+	}
+}
+	
+function isMatch(cardMatch){
+	if (cardMatch[0] === cardMatch[1]){ 
+	alert('You found a match!!');
+	} 
+	else {
+		alert('Sorry try again!');
+	}
+}
+// I can't for the life of me figure out how to reset the cards.
+createCards()
