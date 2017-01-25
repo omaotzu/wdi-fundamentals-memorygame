56b15d3b11c506583fea44eb1bebe9f914027bd8
OmaoTzu <OmaoTzu@Irinas-MBP-2.home>
2017-01-25 17:20:49 +0000

    added functionality

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,4 +1,4 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = ['ace', 'ace', 'king', 'king', 'queen', 'queen', 'jack', 'jack', 'ten', 'ten', 'nine', 'nine', 'eight', 'eight', 'seven', 'seven', 'six', 'six', 'five', 'five', 'four', 'four', 'three', 'three', 'two', 'two'];
 var cardsInPlay = [];
 
 /* if (cardTwo==cardOne) {
@@ -11,8 +11,19 @@ else {
 
 
 var board = document.getElementById('gameBoard');
+
+function shuffleCards () {
+	var i = 0, j=0, temp=null;
+	for (var i=cards.length-1; i>0; i--) {
+		j=Math.floor(Math.random()*(i+1));
+		temp=cards[i];
+		cards[i]=cards[j];
+		cards[j]=temp;
+	}
+} 
 function createCards (){
 		for (var i=0 ; i<cards.length ; i++) {
+			shuffleCards();
 			var cardDiv = document.createElement('div');
 			cardDiv.className = 'card';
 			cardDiv.setAttribute('data-card', cards[i]);
@@ -20,6 +31,11 @@ function createCards (){
 			board.appendChild(cardDiv);
 	}
 }
+/* I stumbled across the Fisher-Yates Shuffle and thought
+	I'd see if it would work in my game. Intention is to add more cards
+	and see if I can have a half deck game of random cards which would be fun.
+	Might have to make the cards a bit smaller though....
+*/
 
 
 
@@ -28,22 +44,66 @@ function isTwoCards(){
 		if (this.getAttribute('data-card')==='queen') {
 			this.innerHTML="<img src='http://i.imgur.com/eO9Qt1K.png'>";
 		}
-		else {
+		else if (this.getAttribute('data-card')==='king') {
 			this.innerHTML="<img src = 'http://i.imgur.com/gExrkx4.png'>";
 		}
+		else if (this.getAttribute('data-card')==='ace') {
+			this.innerHTML="<img src = 'http://i.imgur.com/zlToZnK.png'>";
+	    }
+	    else if (this.getAttribute('data-card')==='jack') {
+			this.innerHTML="<img src = 'http://i.imgur.com/KHyZQUw.png'>";
+	    }
+	    else if (this.getAttribute('data-card')==='ten') {
+			this.innerHTML="<img src = 'http://i.imgur.com/AGt5mbk.png'>";
+	    }
+	    else if (this.getAttribute('data-card')==='nine') {
+			this.innerHTML="<img src = 'http://i.imgur.com/KU6OS6e.png'>";
+	    }
+	    else if (this.getAttribute('data-card')==='eight') {
+			this.innerHTML="<img src = 'http://i.imgur.com/y4RS6C7.png'>";
+	    }
+	    else if (this.getAttribute('data-card')==='seven') {
+			this.innerHTML="<img src = 'http://i.imgur.com/BFDRlOL.png'>";
+	    }
+	    else if (this.getAttribute('data-card')==='six') {
+			this.innerHTML="<img src = 'http://i.imgur.com/leWLZHn.png'>";
+	    }
+	    else if (this.getAttribute('data-card')==='five') {
+			this.innerHTML="<img src = 'http://i.imgur.com/vzCxdrR.png'>";
+	    }
+	    else if (this.getAttribute('data-card')==='four') {
+			this.innerHTML="<img src = 'http://i.imgur.com/He51NfG.png'>";
+	    }
+	    else if (this.getAttribute('data-card')==='three') {
+			this.innerHTML="<img src = 'http://i.imgur.com/rh93gXb.png'>";
+	    }
+	    else if (this.getAttribute('data-card')==='two') {
+			this.innerHTML="<img src = 'http://i.imgur.com/fDGAScI.png'>";
+	    }
 		if(cardsInPlay.length===2) {
-		isMatch(cardsInPlay);
+		setTimeout (isMatch(cardsInPlay) , 1000);
 		cardsInPlay= [];
 	}
 }
-	
+
+function resetCards() {
+		for (var i=0 ; i<cards.length; i++) {
+		if (document.getElementsByClassName('card')[i]) {
+			document.getElementsByClassName('card')[i].innerHTML="";
+		}
+	}
+}
+
 function isMatch(cardMatch){
 	if (cardMatch[0] === cardMatch[1]){ 
 	alert('You found a match!!');
 	} 
 	else {
+		resetCards();
 		alert('Sorry try again!');
+		cardsInPlay= [];
 	}
 }
-// I can't for the life of me figure out how to reset the cards.
-createCards()
+
+createCards();
+shuffleCards();
